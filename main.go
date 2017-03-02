9a3928c51bc3cd6e0fe25ae86711f7277441932f
saninoale@gmail.com <saninoale@gmail.com>
2017-03-02 15:33:21 +0100

    Adding the correct port for development server
    Correct port is :8888 because is redirected by nginx from port :8080.

--- main.go
+++ main.go
@@ -47,5 +47,5 @@ func main() {
 	e.POST(constants.GAME_ENABLE_DISABLE_PATH, gameOwnerController.HandleGameAction)
 	e.POST(constants.GAME_OWNER_GAME_LIST_PATH, gameOwnerController.HandleShowMyGames)
 
-	e.Logger().Print(e.Run(fasthttp.New(":8090")))
+	e.Logger().Print(e.Run(fasthttp.New(":8888")))
 }
