aa0395501636711f1315fe7d28999b4b7ef74e81
saninoale@gmail.com <saninoale@gmail.com>
2017-03-13 01:15:04 +0100

    finished developer entity, starting owner entity

--- controllers/game_owner/game_owner_login.go
+++ controllers/game_owner/game_owner_login.go
@@ -9,29 +9,27 @@ import (
 	"sanino/gamemate/controllers/shared"
 	"sanino/gamemate/models/game_owner/requests"
 	"strconv"
-
-	"github.com/garyburd/redigo/redis"
 )
 
 func registerOwner(RegTry gameOwnerRequests.GameOwnerRegistration) (int64, error) {
-	isLoggable, _, err := checkLogin(gameOwnerRequests.GameOwnerAuth{Email: RegTry.Email, Password: RegTry.Password})
-	if err == nil {
-		return -1, errors.New("Cannot check if user is registered")
+	AuthTry := gameOwnerRequests.GameOwnerAuth{Email: RegTry.Email, Password: RegTry.Password}
+	isLoggable, _, err := checkLogin(AuthTry)
+	if err != nil {
+		return -1, errors.New("Login Error Check => " + err.Error())
 	}
 	if isLoggable {
-		return -1, errors.New("Developer already registered")
+		return -1, errors.New("Owner already registered, cannot register again.")
 	}
 	salt := rand.Intn(constants.MAX_NUMBER_SALT)
-	saltedPass := RegTry.Password + strconv.Itoa(salt)
+	saltedPass := controllerSharedFuncs.ConvertToHexString(RegTry.Password + strconv.Itoa(salt))
 
 	stmtQuery, err := configurations.ArchivesPool.Prepare(
-		fmt.Sprintf("INSERT INTO game_owners (ownerID, email, hash_pwd, hash_salt) VALUES (NULL, ?, %s, ?)",
-			controllerSharedFuncs.ConvertToHexString(saltedPass)),
+		fmt.Sprintf("INSERT INTO game_owners (ownerID, email, hash_pwd, hash_salt) VALUES (NULL, ?, UNHEX(?), ?)"),
 	)
 	if err != nil {
 		return -1, err
 	}
-	result, err := stmtQuery.Exec(RegTry.Email, salt)
+	result, err := stmtQuery.Exec(RegTry.Email, saltedPass, salt)
 	if err != nil {
 		return -1, err
 	}
@@ -71,8 +69,8 @@ func checkLogin(AuthTry gameOwnerRequests.GameOwnerAuth) (bool, int64, error) {
 	if err != nil {
 		return false, -1, err
 	}
-	if !result.Next() {
-		return false, -1, errors.New("Cannot login user, Database error")
+	if !result.Next() { //user not found, so no error
+		return false, -1, nil
 	}
 	err = result.Scan(&num_rows, &password_hash, &salt, &ownerID)
 	if err != nil {
@@ -82,7 +80,7 @@ func checkLogin(AuthTry gameOwnerRequests.GameOwnerAuth) (bool, int64, error) {
 	salted_hash := controllerSharedFuncs.ConvertToHexString(salted_pwd)
 	//fmt.Println("0x" + password_hash)
 	//fmt.Println(salted_hash)
-	if salted_hash == fmt.Sprintf("0x%s", password_hash) {
+	if salted_hash == password_hash {
 		return true, ownerID, nil
 	}
 	return false, -1, nil
@@ -93,15 +91,5 @@ func updateCacheWithSessionOwnerToken(ownerID int64) (string, error) {
 }
 
 func getOwnerIDFromSessionToken(token string) (int64, error) {
-	command := fmt.Sprintf("%s/with_token/%s", constants.LOGGED_OWNERS_SET, token)
-
-	conn := configurations.CachePool.Get()
-	ID, err := redis.Int64(conn.Do("HGET", command, "ID"))
-	if err != nil {
-		return -1, fmt.Errorf("Invalid Session : command = %s, response = %d, error = %v", command, ID, err)
-	}
-	if ID == 0 {
-		return -1, fmt.Errorf("Invalid Session : command = %s, response = %d, error = %v", command, ID, err)
-	}
-	return ID, nil
+	return controllerSharedFuncs.GetIDFromSessionSet(constants.LOGGED_OWNERS_SET, token)
 }
