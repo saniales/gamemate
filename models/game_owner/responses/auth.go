0388724fa93232d4440f15c5b8ced2b1fa9bb91b
saninoale@gmail.com <saninoale@gmail.com>
2017-03-13 01:17:04 +0100

    finished developer entity, starting owner entity

--- models/game_owner/responses/auth.go
+++ models/game_owner/responses/auth.go
@@ -10,6 +10,6 @@ type GameOwnerAuth struct {
 
 //FromToken fills the struct's data with proper definition, based on a session token.
 func (receiver *GameOwnerAuth) FromToken(SessionToken string) {
-	receiver.Type = "Session Token"
+	receiver.Type = "GameOwnerAuth"
 	receiver.SessionToken = SessionToken
 }
