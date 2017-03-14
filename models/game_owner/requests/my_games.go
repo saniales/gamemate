b934a94ce8e6f7c81ea677cc1c04c4900fd074dc
saninoale@gmail.com <saninoale@gmail.com>
2017-03-14 23:11:09 +0100

    owner features

--- models/game_owner/requests/my_games.go
+++ models/game_owner/requests/my_games.go
@@ -7,7 +7,7 @@ import (
 )
 
 //MyGames represents a request to obtain a gameOwnerGameList for the gameOwner.
-type MyGames struct {
+type GameOwnerGameList struct {
 	Type         string `json:"Type" xml:"Type" form:"Type"`
 	API_Token    string `json:"API_Token" xml:"API_Token" form:"API_Token"`
 	SessionToken string `json:"SessionToken" xml:"SessionToken" form:"SessionToken"`
@@ -16,9 +16,9 @@ type MyGames struct {
 //FromForm creates a valid struct based on form data submitted, or returns error.
 //
 // Does not check for the validity of the items inside the struct (e.g. tokens)
-func (receiver *MyGames) FromForm(c echo.Context) error {
+func (receiver *GameOwnerGameList) FromForm(c echo.Context) error {
 	err := c.Bind(receiver)
-	if err != nil || receiver.Type != "MyGames" {
+	if err != nil || receiver.Type != "GameOwnerGameList" {
 		return errors.New("Invalid Form Submitted " + err.Error())
 	}
 	return nil
