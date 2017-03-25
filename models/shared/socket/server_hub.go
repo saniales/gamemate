1bed29ac89e1483a0e788d32458c5cc8ceb42ef1
saninoale@gmail.com <saninoale@gmail.com>
2017-03-25 18:06:57 +0100

    adding channel

--- models/shared/socket/server_hub.go
+++ models/shared/socket/server_hub.go
@@ -6,21 +6,25 @@ import (
 	"sanino/gamemate/models/user/data_structures"
 )
 
+//SocketHub represents a hub with multiple connections to clients.
 type SocketHub struct {
 	Clients map[*websocket.Conn]userDataStructs.Player //Represents the pool of connected clients.
 }
 
+//NewSocketHub creates a new Hub for a room.
 func NewSocketHub() SocketHub {
 	return SocketHub{
 		Clients: make(map[*websocket.Conn]userDataStructs.Player),
 	}
 }
 
+//AddClient adds a connection to the hub, linking it with the player.
 func (receiver *SocketHub) AddClient(client *websocket.Conn, player userDataStructs.Player) {
 	receiver.Clients[client] = player
 }
 
-func (receiver *SocketHub) BroadCast(message interface{}) []error {
+//Broadcast sends to all connected peers a message
+func (receiver *SocketHub) Broadcast(message interface{}) []error {
 	errors := make([]error, 0, 1)
 	for conn, _ := range receiver.Clients {
 		err := conn.WriteJSON(message)
--- models/shared/socket/server_pool.go
+++ models/shared/socket/server_pool.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+//ServerRoom represents a lobby on the server.
 type ServerRoom struct {
 	ID           int64     //ID of the room.
 	hub          SocketHub //Connected users.
@@ -13,14 +14,17 @@ type ServerRoom struct {
 	MatchStarted bool      //Match started.
 }
 
+//IsFull returns true if it is not possible to add other players,
+//false otherwise.
 func (receiver *ServerRoom) IsFull() bool {
 	return receiver.MatchStarted || receiver.PlayersLeft == 0
 }
 
+//NewServerRoom creates a new server room with the specified parameters.
 func NewServerRoom(roomID int64, maxPlayers int64) *ServerRoom {
 	ret := &ServerRoom{
 		ID:           roomID,
-		hub:          SocketHub{},
+		hub:          NewSocketHub(),
 		PlayersLeft:  maxPlayers - 1,
 		MatchStarted: false,
 	}
@@ -32,14 +36,18 @@ func (receiver *ServerRoom) AddPlayer(p userDataStructs.Player, socket *websocke
 	receiver.hub.Clients[socket] = p
 }
 
-func (receiver *ServerRoom) BroadCastMatchStarted() []error {
-	receiver.MatchStarted = true
+//BroadcastRoomUpdate sends a message to update room of all connected peers.
+func (receiver *ServerRoom) BroadcastRoomUpdate(typeOfUpdate string) []error {
+	if typeOfUpdate == "MatchStarted" {
+		receiver.MatchStarted = true
+	}
 	Message := make(map[string]interface{})
+	Message["Action"] = typeOfUpdate
 	Message["RoomID"] = receiver.ID
 	Players := make([]userDataStructs.Player, 0, 0)
 	for _, v := range receiver.hub.Clients {
 		Players = append(Players, v)
 	}
 	Message["Players"] = Players
-	return receiver.hub.BroadCast(Message)
+	return receiver.hub.Broadcast(Message)
 }
