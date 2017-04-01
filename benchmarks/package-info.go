63de2c104f5f458bd6ddbe047bf4ca2db1348013
saninoale@gmail.com <saninoale@gmail.com>
2017-04-01 18:45:45 +0200

    final

---
+++ benchmarks/package-info.go
@@ -0,0 +1,2 @@
+//Package benchmarks contains all the benchmarks to show the performance of the system.
+package benchmarks
---
+++ benchmarks/session_test.go
@@ -0,0 +1,29 @@
+package benchmarks
+
+import (
+	"net/http"
+	"net/url"
+	"runtime"
+	"strconv"
+	"testing"
+)
+
+func BenchmarkDevAuthRequestScalability(b *testing.B) {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	const (
+		API_TOKEN     string = "5D170C3D25E269CE8FC98BEACBAB944F49125AD068EE239D6117A41EBF58B8904EF5F0F4BF747DCC9D46C033FF58E15B2CD0D352CDAACDAEA9FD942E891A88ED"
+		URL           string = "http://gamemate.di.unito.it:8080/dev/auth"
+		USER_TEMPLATE string = "benchmark_user_"
+	)
+	form := url.Values{
+		"Type":      {"DevAuth"},
+		"API_Token": {API_TOKEN},
+		"Password":  {"benchmark"},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		form.Add("Email", USER_TEMPLATE+strconv.Itoa(i))
+		http.PostForm(URL, form)
+		form.Del("Email")
+	}
+}
